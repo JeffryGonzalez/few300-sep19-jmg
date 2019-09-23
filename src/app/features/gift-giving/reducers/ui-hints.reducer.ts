9d29dd102e5b67678aeaaa57f98c4cc8f7907b24
Jeffry Gonzalez <jeff@hypertheory.com>
2019-09-23 19:28:49 +0000

    ah, this is pushit.

--- src/app/features/gift-giving/reducers/ui-hints.reducer.ts
+++ src/app/features/gift-giving/reducers/ui-hints.reducer.ts
@@ -1,19 +1,24 @@
 import { createReducer, Action, on } from '@ngrx/store';
 import * as sortFilterActions from '../actions/sort-filter.actions';
+
 export interface UiHintsState {
   showAll: boolean;
+  sortHolidaysBy: string;
 }
 
 const initialState: UiHintsState = {
-  showAll: true
+  showAll: true,
+  sortHolidaysBy: 'name'
 };
 
 const myReducer = createReducer(
   initialState,
-  on(sortFilterActions.filterShowAll, () => ({ showAll: true })),
-  on(sortFilterActions.filterShowOnlyUpcoming, () => ({ showAll: false }))
+  on(sortFilterActions.filterShowAll, (state) => ({ ...state, showAll: true })),
+  on(sortFilterActions.filterShowOnlyUpcoming, (state) => ({ ...state, showAll: false })),
+  on(sortFilterActions.sortHolidaysByDate, (state) => ({ ...state, sortHolidaysBy: 'date' })),
+  on(sortFilterActions.sortHolidaysByName, (state) => ({ ...state, sortHolidaysBy: 'name' })),
 );
 
-export function reducer(state: UiHintsState, action: Action): UiHintsState {
+export function reducer(state: any, action: any) {
   return myReducer(state, action);
 }
