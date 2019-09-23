08308cd99490e4811f22d62d4c99889d22cde213
Jeffry Gonzalez <jeff@hypertheory.com>
2019-09-23 18:55:51 +0000

    sorting done

---
+++ src/app/features/gift-giving/actions/sort-filter.actions.ts
@@ -0,0 +1,10 @@
+import { createAction } from '@ngrx/store';
+
+export const filterShowAll = createAction(
+  '[gift-giving] filter show all'
+);
+
+
+export const filterShowOnlyUpcoming = createAction(
+  '[gift-giving] filter show only upcoming'
+);
