a1e7bd0b53e2bdf74c5597a70e7633cd3cf02dd2
Jeffry Gonzalez <jeff@hypertheory.com>
2019-09-24 16:50:24 +0000

    after lunch

--- src/app/features/gift-giving/effects/app.effects.ts
+++ src/app/features/gift-giving/effects/app.effects.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { pipe } from 'rxjs';
-import { tap, map } from 'rxjs/operators';
+import { tap, map, switchMap } from 'rxjs/operators';
 import * as appActions from '../../../actions/app.actions';
 import * as sortFilterActions from '../actions/sort-filter.actions';
+import * as holidayActions from '../actions/holidays.actions';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { HolidayEntity } from '../reducers/holidays.reducer';
 @Injectable()
 export class AppEffects {
 
@@ -14,5 +18,17 @@ export class AppEffects {
     ), { dispatch: true }
   );
 
-  constructor(private actions$: Actions) { }
+  onAppStartLoadHolidays$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(appActions.applicationStarted),
+      switchMap(() => this.client.get<{ holidays: HolidayEntity[] }>(environment.holidayUrl)
+        .pipe(
+          map(response => response.holidays),
+          map(holidays => holidayActions.loadDataSucceeded({ data: holidays }))
+        )
+      )
+    ), { dispatch: true }
+  );
+
+  constructor(private actions$: Actions, private client: HttpClient) { }
 }
